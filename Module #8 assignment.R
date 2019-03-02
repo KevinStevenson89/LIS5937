c859d4f2825aae2e1931c3adbb0c88753c502cb4
KevinStevenson89 <46585121+KevinStevenson89@users.noreply.github.com>
2019-03-01 22:55:32 -0500

    Add files via upload
    Updated/Corrected Module #8 Assignment

--- Module #8 assignment.R
+++ Module #8 assignment.R
@@ -7,11 +7,11 @@ x <- read.table(file.choose(), header = TRUE, sep = ",")
 #generate mean for both Age and Grade split by Gender
 students_gendered_mean <- ddply(x,"Sex",transform, Grade.Average=mean(Grade))
 
-#Write x to a file
+#Write students_gendered_mean to a file
 write.table(students_gendered_mean,"Sorted_Average")
 
 #Filter names to only inlcude those containing the letter [iI]
 i_students <- subset(x,grepl("[iI]",x$Name))
 
-#Write x1 to a file
+#Write i_students to a file
 write.table(i_students,"DataSubset",sep=",")
